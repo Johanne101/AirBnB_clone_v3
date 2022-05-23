c6dc9ed1a1ac01c9f3c0865401b4452686b8a1a3
Luijma <rsluismanuel@gmail.com>
2022-05-23 15:55:36 +0000

    Adding City with added code to check for the state and handle any errors that could occur with a stateless city

---
+++ api/v1/views/cities.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+from flask import jsonify, request, abort
+from api.v1.views import app_views
+from models import storage
+from models.city import City
+from werkzeug.exceptions import HTTPException
+
+
+@app_views.route('cities', methods=['POST, GET'] strict_slashes=False)
+def all_cities(state_id):
+    state = storage.get('State', state_id)
+    if state is None:
+        abort(404)
+    if request.method == 'POST':
+        city_created = None
+        try:
+            request = request.get_json()
+            city_name = request.get('name')
+            if city_name is None:
+                abort(400, description='Missing name')
+
+            city_created = City(name=city_name)
+            city_created.save()
+            return jsonify(city_created.to_dict()), 201
+        except Exception as e:
+            if isinstance(e, HTTPException):
+                abort(400, description='Not a JSON')
+            else:
+                abort(500)
+
+        cities = storage.all('City')
+        city_list = []
+        for city in cities.value():
+            city_list.append(city.to_dict())
+        return jsonify(city_list)
+
+
+@app_views.route('/cities/<city_id>',
+                 methods=['GET', 'DELETE', 'PUT'], strict_slashes=False)
+def city_by_id(city_id):
+    city = storage.get('City', city_id)
+    if city is None:
+        abort(404)
+    if request.method == 'DELETE':
+        storage.delete(city)
+        storage.save()
+        return jsonify({}), 200
+    elif request.method == 'PUT':
+        try:
+            request = request.get_json()
+            request['created_at'] = city.created_at
+            request['id'] = city.id
+            city = City(**request)
+            city.save()
+            return jsonify(city.to_dict()), 200
+        except Exception as e:
+            if isinstance(e, HTTPException):
+                abort(400, description='Not a JSON')
+            else:
+                abort(500)
+    return jsonify(city.to_dict())

7b5b11adf6809ec8c42f9dad72823f14ef8c38dc
Luijma <rsluismanuel@gmail.com>
2022-05-23 16:13:29 +0000

    fixed typo in ok status for index for /api/v1/stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,7 +9,7 @@ from models import storage
 
 @app_views.route('/status', strict_slashes=False)
 def status():
-    return jsonify({"status": OK})
+    return jsonify({"status": "OK"})
 
 
 @app_views.route('stats', strict_slashes=False)
